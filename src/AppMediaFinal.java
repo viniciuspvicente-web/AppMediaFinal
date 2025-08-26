dc64eb3bc1f0bb1c788530a1e60bb2ac64db1227
viniciuspvicente-web <vinicius_p_vicente@estudante.sesisenai.org.br>
2025-08-26 15:13:34 -0300

    Aplicação da média inicial
    Coloquei as variáveis e o método de média

---
+++ src/AppMediaFinal.java
@@ -0,0 +1,13 @@
+public class AppMediaFinal {
+    public static void main(String[] args) throws Exception {
+        float n1, n2, n3, mf;
+
+        n1 = 7.8f;
+        n2 = 5.5f;
+        n3 = 4.9f;
+
+        mf = (n1+n2+n3)/3;
+
+        System.out.println("MF: "+mf);
+    }
+}
